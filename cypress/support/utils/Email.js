47511a420109d308851cf2137726922d67608f87
ellatom <68844149+ellatom@users.noreply.github.com>
2021-06-27 22:44:58 +0300

    Update Email.js

--- cypress/support/utils/Email.js
+++ cypress/support/utils/Email.js
@@ -2,15 +2,15 @@
 class Email {
 
 	async sendEmail() {
-		const DOMAIN = "sandbox87b31a1337084192b4efc84126e1cb12.mailgun.org";
-		const api_key = "d1e8231f1809af85c251968b74a7d6d5-24e2ac64-f9ef2521";
+		const DOMAIN = "mydomain.mailgun.org";
+		const api_key = "apikey";
 		const mailgun = require('mailgun-js')({ apiKey: api_key, domain: DOMAIN });
 
 		let filePath = require('../../../mochawesome-report/mochawesome.json');
 
 		let emailInfo = {
-			from: "Mailgun Sandbox <postmaster@sandbox87b31a1337084192b4efc84126e1cb12.mailgun.org>",
-			to: "ella271@gmail.com",
+			from: "Mailgun Sandbox <postmaster@mysandbox.mailgun.org>",
+			to: "myemail",
 			subject: 'Cypress Test Report' + new Date(),
 			text: JSON.stringify(filePath.stats)
 		};
